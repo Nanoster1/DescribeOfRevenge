6072180f3cef99798195756f5d35a6bc3c459817
Slava <sh-e-95@mail.ru>
2022-11-26 15:49:38 +0500

    init charter

--- Character/Character.cs
+++ Character/Character.cs
@@ -4,8 +4,6 @@ public class Character : KinematicBody2D
 {
     public CharacterSystem CharSystem { get; private set; }
     public Sprite PlayerImage { get; private set; }
-    private const float jumpForce = 400;
-    private const float gravity = 9.8f;
 
     public override void _Ready()
     {
@@ -16,30 +14,29 @@ public class Character : KinematicBody2D
     public override void _PhysicsProcess(float delta)
     {
 
-        Vector2 direction = Vector2.Zero;
+        Vector2 direction = Vector2.Down;
+        float gravity = 9.8f;
         if (Input.IsActionPressed("move_left"))
         {
-            direction.x = -CharSystem.Speed;
+            direction.x = -1;
             PlayerImage.FlipH = true;
         }
 
         if (Input.IsActionPressed("move_right"))
         {
-            direction.x = CharSystem.Speed;
+            direction.x = +1;
             PlayerImage.FlipH = false;
         }
 
         if (Input.IsActionPressed("move_up") && IsOnFloor())
         {
-            direction.y -= gravity * CharSystem.Speed;
+            direction.y -= gravity;
         }
 
         if (Input.IsActionPressed("move_down") && IsOnFloor())
         {
-            direction.y += gravity * CharSystem.Speed;
+            direction.y += gravity;
         }
-        direction.y += Mathf.Pow(gravity, 2) * 2;
-        GD.Print(direction);
-        MoveAndSlide(direction, Vector2.Up);
+        MoveAndSlide(direction * CharSystem.Speed, Vector2.Up);
     }
 }
