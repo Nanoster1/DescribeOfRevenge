6e593c4b5ca456d9a0f7d075d6960f08ca2a6067
Slava <sh-e-95@mail.ru>
2022-11-27 17:49:23 +0500

    finish

--- scenes/gui/Menu/MainMenu.cs
+++ scenes/gui/Menu/MainMenu.cs
@@ -1,15 +1,18 @@
 using Godot;
-using System;
 
 public class MainMenu : Control
 {
-	public void OnNewGameButtonPressed()
-	{
-		GetNode<LevelManager>("/root/LevelManager").StartFirstLevel();
-	}
+    public void OnNewGameButtonPressed()
+    {
+        GetNode<LevelManager>("/root/LevelManager").StartFirstLevel();
+    }
 
-	public void _on_AboutButton_pressed()
-	{
-		GetTree().ChangeScene("res://scenes/gui/Menu/CreditsMenu.tscn");
-	}
+    public void _on_AboutButton_pressed()
+    {
+        GetTree().ChangeScene("res://scenes/gui/Menu/CreditsMenu.tscn");
+    }
+    public void _on_QuitButton_pressed()
+    {
+        GetTree().Quit();
+    }
 }
