78327bba9352bb5d61cbdcea562d31b9dec618e8
DemLoveSky <dem.loginov@gmail.com>
2022-11-27 17:37:08 +0500

    Auto stash before merge of "master" and "origin/master"

--- Character/Character.cs
+++ Character/Character.cs
@@ -2,173 +2,173 @@ using Godot;
 
 public class Character : KinematicBody2D
 {
-    public CharacterSystem CharSystem { get; private set; }
-    public AnimatedSprite animatedSprite { get; private set; }
-    private const float gravity = 15f;
-    private const float jumpForce = 350;
-    private Vector2 velocity = Vector2.Zero;
-    private LastDirection? direction = null;
-    private System.Timers.Timer timer = new System.Timers.Timer(4000);
-    private bool isCalm2 = false;
-    private bool isDamage = false;
-    public override void _Ready()
-    {
-        animatedSprite = GetNode<AnimatedSprite>("AnimatedSprites");
-        CharSystem = GetNode<CharacterSystem>(nameof(CharacterSystem));
-        timer.Elapsed += (x, y) =>
-        {
-            isCalm2 = true;
+	public CharacterSystem CharSystem { get; private set; }
+	public AnimatedSprite animatedSprite { get; private set; }
+	private const float gravity = 15f;
+	private const float jumpForce = 350;
+	private Vector2 velocity = Vector2.Zero;
+	private LastDirection? direction = null;
+	private System.Timers.Timer timer = new System.Timers.Timer(4000);
+	private bool isCalm2 = false;
+	private bool isDamage = false;
+	public override void _Ready()
+	{
+		animatedSprite = GetNode<AnimatedSprite>("AnimatedSprites");
+		CharSystem = GetNode<CharacterSystem>(nameof(CharacterSystem));
+		timer.Elapsed += (x, y) =>
+		{
+			isCalm2 = true;
 
-        };
-        timer.AutoReset = false;
-        animatedSprite.Connect("animation_finished", this, "_finish");
-    }
-    public void _finish()
-    {
-        if (animatedSprite.Animation == "Attack")
-        {
-            isDamage = false;
-            GetNode<AudioStreamPlayer2D>("Attack").Playing = false;
-        }
-    }
+		};
+		timer.AutoReset = false;
+		animatedSprite.Connect("animation_finished", this, "_finish");
+	}
+	public void _finish()
+	{
+		if (animatedSprite.Animation == "Attack")
+		{
+			isDamage = false;
+			GetNode<AudioStreamPlayer2D>("Attack").Playing = false;
+		}
+	}
 
-    public override void _PhysicsProcess(float delta)
-    {
-        if (!(animatedSprite.Animation == "Death"))
-        {
-            if (Input.IsActionPressed("move_left"))
-            {
-                velocity.x = -CharSystem.Speed;
-                animatedSprite.FlipH = true;
-                timer.Stop();
-                isCalm2 = false;
-            }
+	public override void _PhysicsProcess(float delta)
+	{
+		if (!(animatedSprite.Animation == "Death"))
+		{
+			if (Input.IsActionPressed("move_left"))
+			{
+				velocity.x = -CharSystem.Speed;
+				animatedSprite.FlipH = true;
+				timer.Stop();
+				isCalm2 = false;
+			}
 
-            else if (Input.IsActionPressed("move_right"))
-            {
-                velocity.x = CharSystem.Speed;
-                animatedSprite.FlipH = false;
-                timer.Stop();
-                isCalm2 = false;
-            }
-            else
-            {
-                velocity.x = 0;
-                if (!timer.Enabled)
-                {
-                    timer.Start();
-                }
+			else if (Input.IsActionPressed("move_right"))
+			{
+				velocity.x = CharSystem.Speed;
+				animatedSprite.FlipH = false;
+				timer.Stop();
+				isCalm2 = false;
+			}
+			else
+			{
+				velocity.x = 0;
+				if (!timer.Enabled)
+				{
+					timer.Start();
+				}
 
-            }
+			}
 
-            if (Input.IsActionJustPressed("move_up") && IsOnFloor())
-            {
-                velocity.y = -jumpForce;
-                direction = LastDirection.Up;
-                timer.Stop();
-                isCalm2 = false;
-                _play_jump_animation();
+			if (Input.IsActionJustPressed("move_up") && IsOnFloor())
+			{
+				velocity.y = -jumpForce;
+				direction = LastDirection.Up;
+				timer.Stop();
+				isCalm2 = false;
+				_play_jump_animation();
 
-            }
+			}
 
-            else if (Input.IsActionJustPressed("move_up") && direction == LastDirection.Up)
-            {
-                velocity.y = -jumpForce;
-                direction = null;
-                timer.Stop();
-                isCalm2 = false;
-                _play_jump_animation();
-            }
-            else if (Input.IsActionJustPressed("move_down") && IsOnFloor())
-            {
-                velocity.y = 1;
-                direction = LastDirection.Down;
-                timer.Stop();
-                isCalm2 = false;
-            }
-            if (Input.IsActionJustPressed("attack"))
-            {
-                animatedSprite.Play("Attack");
-                GetNode<AudioStreamPlayer2D>("Attack").Playing = true;
-                isDamage = true;
-                timer.Stop();
-                isCalm2 = false;
-                var attack = ResourceLoader.Load<PackedScene>("res://Character/DamageArea.tscn");
-                Area2D newGround = attack.Instance<Area2D>();
-                GD.Print(Position);
+			else if (Input.IsActionJustPressed("move_up") && direction == LastDirection.Up)
+			{
+				velocity.y = -jumpForce;
+				direction = null;
+				timer.Stop();
+				isCalm2 = false;
+				_play_jump_animation();
+			}
+			else if (Input.IsActionJustPressed("move_down") && IsOnFloor())
+			{
+				velocity.y = 1;
+				direction = LastDirection.Down;
+				timer.Stop();
+				isCalm2 = false;
+			}
+			if (Input.IsActionJustPressed("attack"))
+			{
+				animatedSprite.Play("Attack");
+				GetNode<AudioStreamPlayer2D>("Attack").Playing = true;
+				isDamage = true;
+				timer.Stop();
+				isCalm2 = false;
+				var attack = ResourceLoader.Load<PackedScene>("res://Character/DamageArea.tscn");
+				Area2D newGround = attack.Instance<Area2D>();
+				GD.Print(Position);
 
-                AddChild(newGround);
+				AddChild(newGround);
 
-                if (animatedSprite.FlipH)
-                {
-                    newGround.Position = new Vector2(newGround.Position.x - 50, newGround.Position.y);
-                }
-                else
-                {
-                    newGround.Position = new Vector2(newGround.Position.x + 50, newGround.Position.y);
-                }
-            }
-            else
-            {
-                _play_animation(velocity);
+				if (animatedSprite.FlipH)
+				{
+					newGround.Position = new Vector2(newGround.Position.x - 50, newGround.Position.y);
+				}
+				else
+				{
+					newGround.Position = new Vector2(newGround.Position.x + 50, newGround.Position.y);
+				}
+			}
+			else
+			{
+				_play_animation(velocity);
 
-            }
-            if (!GetNode<AudioStreamPlayer2D>("Run").Playing)
-            {
-                GetNode<AudioStreamPlayer2D>("Run").Playing = true;
-            }
+			}
+			if (!GetNode<AudioStreamPlayer2D>("Run").Playing)
+			{
+				GetNode<AudioStreamPlayer2D>("Run").Playing = true;
+			}
 
-            velocity.y += gravity;
-            velocity = MoveAndSlide(velocity, new Vector2(0, -1));
-        }
-    }
-    public void _play_jump_animation()
-    {
-        if (!isDamage)
-        {
-            animatedSprite.Play("jump");
-            GetNode<AudioStreamPlayer2D>("Run").Playing = false;
-        }
-    }
-    public void _play_animation(Vector2 direction)
-    {
-        if (!isDamage)
-        {
-            if (direction == Vector2.Zero && IsOnFloor())
-            {
-                if (isCalm2)
-                {
-                    animatedSprite.Play("calm2");
-                }
-                else
-                {
-                    animatedSprite.Play("calm");
-                }
-                GetNode<AudioStreamPlayer2D>("Run").Playing = false;
-            }
-            else if (direction.y > 10)
-            {
-                animatedSprite.Play("down");
-                GetNode<AudioStreamPlayer2D>("Run").Playing = false;
-            }
-            else if (direction.y == 0)
-            {
-                animatedSprite.Play("run");
-                if (!GetNode<AudioStreamPlayer2D>("Run").Playing)
-                {
-                    GetNode<AudioStreamPlayer2D>("Run").Playing = true;
-                }
+			velocity.y += gravity;
+			velocity = MoveAndSlide(velocity, new Vector2(0, -1));
+		}
+	}
+	public void _play_jump_animation()
+	{
+		if (!isDamage)
+		{
+			animatedSprite.Play("jump");
+			GetNode<AudioStreamPlayer2D>("Run").Playing = false;
+		}
+	}
+	public void _play_animation(Vector2 direction)
+	{
+		if (!isDamage)
+		{
+			if (direction == Vector2.Zero && IsOnFloor())
+			{
+				if (isCalm2)
+				{
+					animatedSprite.Play("calm2");
+				}
+				else
+				{
+					animatedSprite.Play("calm");
+				}
+				GetNode<AudioStreamPlayer2D>("Run").Playing = false;
+			}
+			else if (direction.y > 10)
+			{
+				animatedSprite.Play("down");
+				GetNode<AudioStreamPlayer2D>("Run").Playing = false;
+			}
+			else if (direction.y == 0)
+			{
+				animatedSprite.Play("run");
+				if (!GetNode<AudioStreamPlayer2D>("Run").Playing)
+				{
+					GetNode<AudioStreamPlayer2D>("Run").Playing = true;
+				}
 
-            }
-        }
+			}
+		}
 
-    }
+	}
 
-    protected override void Dispose(bool disposing)
-    {
-        base.Dispose(disposing);
-        timer.Dispose();
-    }
+	protected override void Dispose(bool disposing)
+	{
+		base.Dispose(disposing);
+		timer.Dispose();
+	}
 }
 enum LastDirection
 {
--- levels/Level1.cs
+++ levels/Level1.cs
@@ -4,68 +4,68 @@ public class Level1 : Node2D
 {
 	public static int DeathCounter = 0;
 
-    public override void _Ready()
-    {
-        GetNode<Character>("Character")
-            .GetNode<CharacterSystem>("CharacterSystem").Connect(nameof(CharacterSystem.HealthChanged), this, nameof(OnHealthChanged));
-    }
-    public void _on_Enemy_health_depleted(int health)
-    {
-        GetNode<Character>("Character")
-            .GetNode<CharacterSystem>("CharacterSystem").TakeDamage(health);
-    }
-    public void _on_Area2D_body_exited(Node body)
-    {
-        if (body is Character)
-        {
-            Death();
-        }
-    }
+	public override void _Ready()
+	{
+		GetNode<Character>("Character")
+			.GetNode<CharacterSystem>("CharacterSystem").Connect(nameof(CharacterSystem.HealthChanged), this, nameof(OnHealthChanged));
+	}
+	public void _on_Enemy_health_depleted(int health)
+	{
+		GetNode<Character>("Character")
+			.GetNode<CharacterSystem>("CharacterSystem").TakeDamage(health);
+	}
+	public void _on_Area2D_body_exited(Node body)
+	{
+		if (body is Character)
+		{
+			Death();
+		}
+	}
 
-    public void Death()
-    {
-        if (DeathCounter > 0)
-        {
-            GetNode<LevelManager>("/root/LevelManager").StartFirstLevel();
-            DeathCounter = 0;
-            return;
-        }
-        DeathCounter++;
-        GetTree().ReloadCurrentScene();
-    }
+	public void Death()
+	{
+		if (DeathCounter > 0)
+		{
+			GetNode<LevelManager>("/root/LevelManager").StartFirstLevel();
+			DeathCounter = 0;
+			return;
+		}
+		DeathCounter++;
+		GetTree().ReloadCurrentScene();
+	}
 
-    public void OnHealthChanged(int oldHealth, int newHealth)
-    {
-        if (newHealth <= 0)
-        {
-            var character = GetNode<Character>("Character");
-            character.GetNode<AnimatedSprite>("AnimatedSprites").Play("Death");
-            var rnd = new System.Random();
-            var value = rnd.Next(1, 3);
-            if (value == 1)
-            {
-                character.GetNode<AudioStreamPlayer2D>("GoToMother").Play();
-            }
-            else
-            {
-                character.GetNode<AudioStreamPlayer2D>("Die").Play();
-            }
+	public void OnHealthChanged(int oldHealth, int newHealth)
+	{
+		if (newHealth <= 0)
+		{
+			var character = GetNode<Character>("Character");
+			character.GetNode<AnimatedSprite>("AnimatedSprites").Play("Death");
+			var rnd = new System.Random();
+			var value = rnd.Next(1, 3);
+			if (value == 1)
+			{
+				character.GetNode<AudioStreamPlayer2D>("GoToMother").Play();
+			}
+			else
+			{
+				character.GetNode<AudioStreamPlayer2D>("Die").Play();
+			}
 
-            var timer = new System.Timers.Timer(3000);
-            timer.Elapsed += (x, y) =>
-            {
-                Death();
-                timer.Dispose();
-            };
-            timer.Start();
-        }
-        var healthInfo = GetNode<Character>("Character")
-            .GetNode<CanvasLayer>("CanvasLayer")
-                .GetNode<NinePatchRect>("NinePatchRect")
-                    .GetNode<NinePatchRect>("HealthHUDBar");
+			var timer = new System.Timers.Timer(3000);
+			timer.Elapsed += (x, y) =>
+			{
+				Death();
+				timer.Dispose();
+			};
+			timer.Start();
+		}
+		var healthInfo = GetNode<Character>("Character")
+			.GetNode<CanvasLayer>("CanvasLayer")
+				.GetNode<NinePatchRect>("NinePatchRect")
+					.GetNode<NinePatchRect>("HealthHUDBar");
 
-        healthInfo.GetNode<TextureProgress>("TextureProgress").Value = newHealth;
-        healthInfo.GetNode<NinePatchRect>("HealthInfo")
-            .GetNode<Label>("Label2").Text = newHealth.ToString();
-    }
+		healthInfo.GetNode<TextureProgress>("TextureProgress").Value = newHealth;
+		healthInfo.GetNode<NinePatchRect>("HealthInfo")
+			.GetNode<Label>("Label2").Text = newHealth.ToString();
+	}
 }
